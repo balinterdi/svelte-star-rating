1a2cdd66263cead5b0de0a258031fe959490c5f0
Balint Erdi <balint.erdi@gmail.com>
2019-08-20 21:11:56 +0200

    Rating is passed in but doesn't get rerun

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+	import StarRating from './StarRating.svelte';
+	let rating = 5;
+</script>
+
+<label>
+	<input type="range" name="rating" min="0" max="5" step="1" bind:value={rating}>
+	Rating
+</label>
+
+<StarRating rating={rating} />
---
+++ src/StarRating.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let rating = 0;
+  export let maxRating = 5;
+  
+  function stars() {
+    let starsArray = [];
+    for (let i=1; i <= maxRating; i++) {
+      starsArray.push({ rating: i, full: Math.round(rating) >= i });
+    }
+    return starsArray;
+  }
+</script>
+
+{#each stars() as { rating, full }}
+  <span>{full ? 'F' : 'E' }</span>
+{/each}
